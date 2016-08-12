f3c3d7a6301b900fc1a01dc94f2b55dfcd6bf071
TUNG <tunghuynh@MACs-MacBook-Pro-3.local>
2016-08-13 02:08:49 +0700

    Add main view with nav

---
+++ flashcard/flashcard/MathematicsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MathematicsViewController.h
+//  flashcard
+//
+//  Created by TUNG on 8/13/16.
+//  Copyright © 2016 pqhuy1987. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MathematicsViewController : ViewController
+
+@end
