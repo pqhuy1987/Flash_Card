d8f86d6c19cc3e6032d02278bd85dcc33b82385f
Trung Tran <trung.tran@oncam.com>
2016-08-20 18:16:19 +0700

    Update View 2

---
+++ flashcard/flashcard/AlgebraViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlgebraViewController.h
+//  flashcard
+//
+//  Created by HTK-iMac on 8/20/16.
+//  Copyright © 2016 pqhuy1987. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlgebraViewController : UIViewController
+
+@end
---
+++ flashcard/flashcard/cardsViewController.m
@@ -0,0 +1,43 @@
+//
+//  cardsViewController.m
+//  flashcard
+//
+//  Created by HTK-iMac on 8/20/16.
+//  Copyright © 2016 pqhuy1987. All rights reserved.
+//
+
+#import "cardsViewController.h"
+
+@interface cardsViewController ()
+
+@end
+
+@implementation cardsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupView];
+    // Do any additional setup after loading the view.
+}
+
+- (void)setupView {
+    self.title = @"Flash Card";
+    [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
